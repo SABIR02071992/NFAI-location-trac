491a5f78ab4b7c26b81dfac1bd0b27ce49ed3713
sabir <shabiransari671@gmail.com>
2025-12-28 17:30:59 +0530

    add storage functionality

---
+++ lib/src/features/storage/KStorage.dart
@@ -0,0 +1,22 @@
+
+class KStorageKey{
+  KStorageKey._();
+
+  static const userName = "USER_NAME";
+  static const authorizationToken = 'access_token';
+  static const empId="EMP_ID";
+  static const annualLeaveId="ANNUAL_LEAVE_ID";
+  static const userId="User_Id";
+  static const designation="DESIGNATION";
+  static const employeeCode="EMP_CODE";
+  static const employeeName="EMP_NAME";
+  static const employeeGender="EMP_GENDER";
+  static const employeeMobile="EMP_MOBILE";
+  static const employeeEmail="EMP_EMAIL";
+  static const employeeDOB="EMP_DOB";
+  static const employeeAnniversary="EMP_ANNIVERSARY";
+  static const employeeAddress="EMP_ADDRESS";
+  static const tWorkingHrsInTHisMonth="WORKING_HRS_IN_THIS_MONTH";
+  static const leaveTakenInThisMonth="LEAVE_TAKEN_IN_THIS_MONTH";
+
+}
